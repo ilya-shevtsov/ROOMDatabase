94418feb1192a1c6185db4f4c06becee0b63dfe2
ilya-shevtsov <Dayzilya@gmail.com>
2021-03-25 18:39:36 +0300

    added data is now visible in ListFragment

--- app/src/main/java/com/example/roomapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/roomapp/fragments/list/ListFragment.kt
@@ -15,12 +15,24 @@ import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
 
+    private lateinit var userViewModel: UserViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        val adapter = ListAdapter()
+        val recyclerView = view.recyclerview
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        userViewModel.readAllData.observe(viewLifecycleOwner, Observer { user ->
+            adapter.setData(user)
+        })
 
         view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
